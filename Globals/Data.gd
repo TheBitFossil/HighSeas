389cb14e76df45571270c377d57dcfaa0c3a8423
TheBitFossil <8717392-gearguard@users.noreply.gitlab.com>
2025-06-20 15:33:25 +0200

    wip level design ui design

--- Globals/Data.gd
+++ Globals/Data.gd
@@ -1,1 +1,6 @@
 extends Node
+
+const MAX_HP : float = 1
+var hp : int = 1
+const  MAX_CREW : float = 5
+var crew : int = 5
--- UI/HUD.gd
+++ UI/HUD.gd
@@ -1,16 +1,22 @@
 extends CanvasLayer
 
-@onready var hp: ProgressBar = %HProgressBar_HP
-@onready var crew: ProgressBar = %HProgressBar_Crew
+@onready var hp: ProgressBar = %ProgressBar_Hp
+@onready var crew: TextureProgressBar = %TextureProgressBar_Crew
 
 
 func _ready() -> void:
-	hp.max_value = health
-	hp.value = health
+	hp.max_value = Data.MAX_HP
+	hp.value = Data.hp
+	hp.step = .1
+	
+	crew.max_value = Data.MAX_CREW
+	crew.value = Data.crew
+	crew.step = 1
 
 
 func _on_health_changed(value : int):
 	hp.value = value
+
 
 func _on_crew_changed(value : int):
 	crew.value = value
